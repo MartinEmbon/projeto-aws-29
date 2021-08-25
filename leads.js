73712f0629758a5d5bda36c5cf786928e29486bf
Martincho <martinembon@hotmail.com>
2021-08-25 07:57:35 -0300

    leadsUrl

---
+++ leads.js
@@ -0,0 +1,17 @@
+// Axios Leads Routes
+// All routes tested with POSTMAN
+
+//Database name: leads-control
+//Database primary key: emailId (campo obrigatorio)
+
+// GET ALL LEADS: https://89xudou1d2.execute-api.sa-east-1.amazonaws.com/prod/leads
+
+//GET ONE LEAD: https://89xudou1d2.execute-api.sa-east-1.amazonaws.com/prod/lead/emailId/:1002
+//Example: https://89xudou1d2.execute-api.sa-east-1.amazonaws.com/prod/lead?emailId=01
+
+
+// POST: https://89xudou1d2.execute-api.sa-east-1.amazonaws.com/prod/lead
+
+
+// DELETE: https://89xudou1d2.execute-api.sa-east-1.amazonaws.com/prod/lead
+//Example: https://89xudou1d2.execute-api.sa-east-1.amazonaws.com/prod/lead?emailId=01
